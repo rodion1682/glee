c99414decacd3e536db3d15f3c1a8eff3c21c929
Rodion <rodiontropkin17@gmail.com>
2021-01-29 14:55:17 +0200

    added product list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
 $(function(){
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
